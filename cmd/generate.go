703b1237d9e4162061e0a8e97a8a25b0c12bd9e0
Jad <j.hamwi@centroidsol.com>
2023-02-19 14:21:10 +0200

    generate command done

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,1 +1,37 @@
 package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/sethvargo/go-password/password"
+	"github.com/spf13/cobra"
+)
+
+var length int
+var digits int
+var symbols int
+var uppercase bool
+var repeat bool
+
+func init() {
+
+	generateCommand.Flags().IntVarP(&length, "characters", "c", 64, "NUMBER OF CHARACTERS")
+	generateCommand.Flags().IntVarP(&digits, "digits", "d", 10, "NUMBER OF DIGITS")
+	generateCommand.Flags().IntVarP(&symbols, "symbols", "s", 10, "NUMBER OF SYMBOLS")
+	generateCommand.Flags().BoolVarP(&uppercase, "uppercase", "u", false, "ALLOW UPPERCASE")
+	generateCommand.Flags().BoolVarP(&repeat, "repeat", "r", false, "ALLOW REPEAT CHARACTERS")
+	RootCmd.AddCommand(generateCommand)
+}
+
+var generateCommand = &cobra.Command{
+	Use:   "generate",
+	Short: "generate password",
+	Run: func(cmd *cobra.Command, args []string) {
+		password, err := password.Generate(length, digits, symbols, uppercase, repeat)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(password)
+	},
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/sethvargo/go-password v0.2.0 // indirect
 	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
